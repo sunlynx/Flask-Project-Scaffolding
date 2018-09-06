eda62ed174eb0efefe6918b95e3c3c91e84e5b06
gautam-s04 <gautam.bhadauria@crownstack.com>
2018-09-06 13:17:31 +0530

    Added Migrations

--- database/initializer.py
+++ database/initializer.py
@@ -1,3 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate(db=db)
+
+"""
+Below are the commands used to run migrations and other operations
+"""
+# flask db init
+# flask db migrate
+# flask db upgrade
+# flask db --help
--- run.py
+++ run.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from config.initializer import config
 from routes.initializer import router
-from database.initializer import db
+from database.initializer import db, migrate
 from flask import render_template
 import os
 
+"""
+create all initialization of app here.
+To change the configuration quickly
+"""
 
 app = Flask(__name__)
 app.config.from_object(config[os.getenv('FLASK_ENV')])
 router.init_app(app)
 db.init_app(app)
+migrate.init_app(app)
 
 
 @app.route('/')
